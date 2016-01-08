08dbfc3446533339d60f98a14a54d458148072ab
EoinGorman <eoingorman@live.ie>
2016-01-08 18:01:12 +0000

    Boids and Flock classes added from flocking lab

--- Boids/main.cpp
+++ Boids/main.cpp
@@ -7,6 +7,9 @@
 #include "tiledBackground.h"
 #include "Spaceship.h"
 #include "Playership.h"
+#include "Boid.h"
+#include "Pvector.h"
+#include "Flock.h"
 
 using namespace std;
 
@@ -40,6 +43,9 @@ void ClampCamera(sf::View* view, float windowWidth, float windowHeight)
 
 int main()
 {
+	float boidsSize = 3;
+	string action = "flock";
+
 	//Gets the resolution, size, and bits per pixel for the screen of the PC that is running this program.
 	sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
 	const int window_height = desktop.height;
@@ -61,6 +67,30 @@ int main()
 	TiledBackground background(sf::Vector2f(3,3), sf::Vector2f(window_width, window_height));
 
 	Playership player(sf::Vector2f(window_width/2, window_height/2), sf::Vector2f(1, 0), sf::FloatRect(-window_width, -window_height, window_width * 3, window_height * 3));
+
+	//Create flock, vector of shapes, and initialize boids
+	Flock flock;
+	vector<sf::CircleShape> shapes;
+
+	for (int i = 0; i < 50; i++) //Number of boids is hardcoded for testing pusposes.
+	{
+		//Boid b(rand() % window_width, rand() % window_height); //Starts the boid with a random position in the window.
+		Boid b(window_width / 3, window_height / 3); //Starts all boids in the center of the screen
+		sf::CircleShape shape(8, 3); //Shape with a radius of 10 and 3 points (Making it a triangle)
+
+									 //Changing the Visual Properties of the shape
+									 //shape.setPosition(b.location.x, b.location.y); //Sets position of shape to random location that boid was set to.
+		shape.setPosition(window_width, window_height); //Testing purposes, starts all shapes in the center of screen.
+		shape.setOutlineColor(sf::Color(0, 255, 0));
+		shape.setFillColor(sf::Color::Green);
+		shape.setOutlineColor(sf::Color::White);
+		shape.setOutlineThickness(1);
+		shape.setRadius(boidsSize);
+
+		//Adding the boid to the flock and adding the shapes to the vector<sf::CircleShape>
+		flock.addBoid(b);
+		shapes.push_back(shape);
+	}
 
 	while (window.isOpen())
 	{	
@@ -86,7 +116,13 @@ int main()
 		}
 
 		//UPDATE HERE!!! 
-		
+		if (InputManager::GetInstance()->IsKeyDown(sf::Keyboard::Space))
+		{
+			if (action == "flock")
+				action = "swarm";
+			else
+				action = "flock";
+		}
 		player.Update(deltaClock.getElapsedTime().asSeconds());
 
 		deltaClock.restart();
@@ -104,6 +140,44 @@ int main()
 		window.setView(standard);
 		background.Draw(&window);
 		player.Draw(&window);
+
+		//Draws all of the Boids out, and applies functions that are needed to update.
+		for (int i = 0; i < shapes.size(); i++)
+		{
+			window.draw(shapes[i]);
+
+			//Cout's removed due to slowdown and only needed for testing purposes
+			//cout << "Boid "<< i <<" Coordinates: (" << shapes[i].getPosition().x << ", " << shapes[i].getPosition().y << ")" << endl;
+			//cout << "Boid Code " << i << " Location: (" << flock.getBoid(i).location.x << ", " << flock.getBoid(i).location.y << ")" << endl;
+
+			//Matches up the location of the shape to the boid
+			shapes[i].setPosition(flock.getBoid(i).location.x, flock.getBoid(i).location.y);
+
+			// Calculates the angle where the velocity is pointing so that the triangle turns towards it.
+			float theta;
+			theta = flock.getBoid(i).angle(flock.getBoid(i).velocity);
+			shapes[i].setRotation(theta);
+
+			// These if statements prevent boids from moving off the screen through warpping
+			// If boid exits right boundary
+			if (shapes[i].getPosition().x > window_width)
+				shapes[i].setPosition(shapes[i].getPosition().x - window_width, shapes[i].getPosition().y);
+			// If boid exits bottom boundary
+			if (shapes[i].getPosition().y > window_height)
+				shapes[i].setPosition(shapes[i].getPosition().x, shapes[i].getPosition().y - window_height);
+			// If boid exits left boundary
+			if (shapes[i].getPosition().x < 0)
+				shapes[i].setPosition(shapes[i].getPosition().x + window_width, shapes[i].getPosition().y);
+			// If boid exits top boundary
+			if (shapes[i].getPosition().y < 0)
+				shapes[i].setPosition(shapes[i].getPosition().x, shapes[i].getPosition().y + window_height);
+		}
+
+		//Applies the three rules to each boid in the flock and changes them accordingly.
+		if (action == "flock")
+			flock.flocking();
+		else
+			flock.swarming();
 
 		//Updates the window with current values of any data that was modified.
 		window.display();
